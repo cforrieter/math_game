4f18d6c7255ee5755ba852417cc3ed018134f888
cforrieter <cforrieter@gmail.com>
2015-09-04 19:39:17 +0000

    Refactored into classes

--- math_game.rb
+++ math_game.rb
@@ -1,142 +1,146 @@
 require 'colorize'
 
-@player1 = {
-  :wins => 0,
-  :lives => 3,
-  :name => "Player1"
-}
-@player2 = {
-  :wins => 0,
-  :lives => 3,
-  :name => "Player2"
-}
+class Player
+  attr_accessor :lives
+  attr_reader :name, :wins
 
-def initialize_players
-
-  puts "What is player 1's name?"
-  @player1[:name] = gets.chomp.capitalize
-
-  puts "What is player 2's name?"
-  @player2[:name] = gets.chomp.capitalize
-end
-
-def initialize_game
-  @player1[:lives] = 3
-  @player2[:lives] = 3
-  player = @player1
-  game_status(player)
-  
-  return player
-end
-
-def win_status
-  "\n\n#{@player1[:name]} has won #{@player1[:wins]} times!\n#{@player2[:name]} has won #{@player2[:wins]} times!"
-end
-
-def game_status(player)
-  game_over = false
-  message = ''
-  unless player[:lives] === 0
-    message = "The current game status is: \n\t#{@player1[:name]}: #{@player1[:lives]}\n\t#{@player2[:name]}: #{@player2[:lives]}"
-  else
-    game_over = true
-    message << "The game is over!\n"
-    if player != @player1
-      message << "#{@player1[:name]} wins with #{@player1[:lives]} live(s) remaining."
-      @player1[:wins] += 1 
-    else
-      message << "#{@player2[:name]} wins with #{@player2[:lives]} live(s) remaining."
-      @player2[:wins] += 1 
-    end
-    message << win_status
+  def initialize(name)
+    @name = name
+    @wins = 0
   end
-  puts message
-  return game_over
+
+  def lose_life
+    @lives -= 1
+  end
+
+  def gain_win
+    @wins += 1
+  end
 end
 
-def lose_life(player)
-  player[:lives] -= 1
-end
+class Game
 
-def swap_players(player)
-  if player === @player1
-    player = @player2
-  else
+  def self.initialize_players
+    puts "What is player 1's name?"
+    name1 = gets.chomp.capitalize
+
+    puts "What is player 2's name?"
+    name2 = gets.chomp.capitalize
+
+    return Game.new(name1, name2)
+  end
+  
+  def initialize(name1, name2)
+    @player1 = Player.new(name1)
+    @player2 = Player.new(name2)
+  end
+
+
+  def start_game
+    finished = false
+    until finished
+      game_over = false
+      player = new_game
+      until game_over
+        turn(player)
+        game_over = game_over?(player)
+        player = swap_players(player)
+        if game_over
+          player.gain_win
+          puts game_summary(player)
+        end
+      end
+      puts "Play again? (y/n)"
+      play_again = gets.chomp.downcase
+      case play_again
+      when 'n'
+        finished = true
+      end
+    end
+  end
+
+  def new_game
+    @player1.lives = 3
+    @player2.lives = 3
+    puts game_status
     player = @player1
   end
-end
 
-def generate_question
-  num1 = rand(20) + 1
-  num2 = rand(20) + 1
-  operation = rand(4) + 1
-  case operation
-  when 1
-    operation = "+"
-  when 2
-    operation = "-"
-    if num1 < num2
-      num1, num2 = num2, num1
-    end
-  when 3
-    operation = "*"
-  when 4
-    operation = "/"
-    if num1 < num2
-      num1, num2 = num2, num1
+  def turn(player)
+    question, answer = generate_question_with_answer
+    player_answer = prompt_player(player, question)
+    unless verify_answer(player, answer, player_answer)
+      player.lose_life
+      puts game_status
     end
   end
-  
-  question = "what is #{num1} #{operation} #{num2}?"
-  answer = eval "#{num1} #{operation} #{num2}"
-  return [question, answer]
-end
 
-def prompt_player(player, question)
-  puts "#{player[:name]}, #{question}"
-  print "You answer: "
-  return gets.chomp.to_i
-end
-
-def verify_answer(player, answer, player_answer)
-  if answer == player_answer
-    puts "Correct!".colorize(:green)
-    return true
-  else
-    puts "Wrong! The answer was #{answer}.".colorize(:red)
-    return false
+  def random_number(max)
+    rand(max) + 1
   end
-end
 
-def turn(player)
-  game_over = false
-  question, answer = generate_question
-  player_answer = prompt_player(player, question)
-  unless verify_answer(player, answer, player_answer)
-    lose_life(player)
-    game_over = game_status(player)
-  end
-  return game_over
-end
-
-def game
-  finished = false
-  initialize_players
-
-  until finished
-    game_over = false
-    player = initialize_game
-    until game_over
-      game_over = turn(player)
-      player = swap_players(player)
+  def generate_question_with_answer
+    num1 = random_number(20)
+    num2 = random_number(20)
+    operation = random_number(4)
+    case operation
+    when 1
+      operation = "+"
+    when 2
+      operation = "-"
+      if num1 < num2
+        num1, num2 = num2, num1
+      end
+    when 3
+      operation = "*"
+    when 4
+      operation = "/"
+      if num1 < num2
+        num1, num2 = num2, num1
+      end
     end
-    puts "Play again? (y/n)"
-    play_again = gets.chomp.downcase
-    case play_again
-    when 'n'
-      finished = true
+    
+    question = "what is #{num1} #{operation} #{num2}?"
+    answer = eval "#{num1} #{operation} #{num2}"
+    return [question, answer]
+  end
+
+  def prompt_player(player, question)
+    puts "#{player.name}, #{question}"
+    print "You answer: "
+    return gets.chomp.to_i
+  end
+
+  def verify_answer(player, answer, player_answer)
+    if answer == player_answer
+      puts "\nCorrect!\n".colorize(:green)
+      return true
+    else
+      puts "\nWrong! The answer was #{answer}.\n".colorize(:red)
+      return false
     end
   end
+
+  def game_over?(player)
+    player.lives === 0
+  end
+
+
+  def game_summary(winning_player)
+    "The game is over!\n#{winning_player.name} wins with #{winning_player.lives} live(s) remaining.\n\n#{@player1.name} has won #{@player1.wins} times!\n#{@player2.name} has won #{@player2.wins} times!\n".colorize(:blue)
+  end
+
+  def game_status
+      "The current game status is: \n\t#{@player1.name}: #{@player1.lives}\n\t#{@player2.name}: #{@player2.lives}\n\n"
+  end
+
+  def swap_players(player)
+    player === @player1 ? @player2 : @player1
+  end
+
+
 end
 
-game
+
+game1 = Game.initialize_players
+game1.start_game
